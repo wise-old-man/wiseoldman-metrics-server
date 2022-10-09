b4d6233775e37f582caf828ecfa9307a2de0f071
Psikoi <ruben.amendoeira@gmail.com>
2022-10-09 18:24:21 +0200

    Add API v2 metrics collection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const APIExporter = require("./exporters/APIExporter");
 const config = require("./config");
+const APIExporter = require("./exporters/APIExporter");
+const APIV2Exporter = require("./exporters/APIV2Exporter");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 setInterval(() => APIExporter.collect(), config.api.collectionTimeout);
+setInterval(() => APIV2Exporter.collect(), config["api-v2"].collectionTimeout);
 
 app.get("/metrics/api", async (req, res) => {
   const metrics = await APIExporter.getMetrics();
+  res.end(metrics);
+});
+
+app.get("/metrics/api-v2", async (req, res) => {
+  const metrics = await APIV2Exporter.getMetrics();
   res.end(metrics);
 });
 
