98490a53eac1842ad2d84849b6a9fdfd910045d4
Psikoi <ruben.amendoeira@gmail.com>
2023-01-04 11:31:27 +0100

    Remove v2 exporter reference

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const config = require("./config");
 const APIExporter = require("./exporters/APIExporter");
-const APIV2Exporter = require("./exporters/APIV2Exporter");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 setInterval(() => APIExporter.collect(), config.api.collectionTimeout);
-setInterval(() => APIV2Exporter.collect(), config["api-v2"].collectionTimeout);
 
 app.get("/metrics/api", async (req, res) => {
   const metrics = await APIExporter.getMetrics();
