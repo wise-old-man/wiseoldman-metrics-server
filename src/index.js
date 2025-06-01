46aadc0e1ac65d7a0d4748af728613185b724441
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2025-06-01 03:20:19 +0200

    Fix payload size limit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const LeagueAPIExporter = require("./exporters/LeagueAPIExporter");
 const MetricSource = require("./MetricSource");
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const PORT = process.env.PORT || 3500;
 
